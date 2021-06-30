d522da6a8fb513c3b6e6a748296354dd3945cc54
Sean-Black <MailSeanBlack@Yahoo.com>
2021-06-30 11:44:20 +0100

    New commit

--- utils/requests.js
+++ utils/requests.js
@@ -5,7 +5,7 @@ export default {
     title: "Trending",
     url: `trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
-  fetchTrending: {
+  fetchTopRated: {
     title: "Top Rated",
     url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   },
@@ -15,19 +15,19 @@ export default {
   },
   fetchComedyMovies: {
     title: "Comedy",
-    url: `/discover/movies?api_key=${API_KEY}&&with_genres=35`,
+    url: `/discover/movies?api_key=${API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: "Horror",
-    url: `/discover/movies?api_key=${API_KEY}&&with_genres=27`,
+    url: `/discover/movies?api_key=${API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: "Romance",
-    url: `/discover/movies?api_key=${API_KEY}&&with_genres=10749`,
+    url: `/discover/movies?api_key=${API_KEY}&with_genres=10749`,
   },
   fetchMysteryMovies: {
     title: "Mystery",
-    url: `/discover/movies?api_key=${API_KEY}&&with_genres=9648`,
+    url: `/discover/movies?api_key=${API_KEY}&with_genres=9648`,
   },
   fetchSciFiMovies: {
     title: "SciFi",
@@ -35,14 +35,14 @@ export default {
   },
   fetchWesternMovies: {
     title: "Western",
-    url: `/discover/movies?api_key=${API_KEY}&&with_genres=37`,
+    url: `/discover/movies?api_key=${API_KEY}&with_genres=37`,
   },
   fetchAnimationMovies: {
     title: "Animation",
-    url: `/discover/movies?api_key=${API_KEY}&&with_genres=16`,
+    url: `/discover/movies?api_key=${API_KEY}&with_genres=16`,
   },
   fetchTV: {
     title: "TVMovie",
-    url: `/discover/movies?api_key=${API_KEY}&&with_genres10770`,
+    url: `/discover/movies?api_key=${API_KEY}&with_genres10770`,
   },
 };
